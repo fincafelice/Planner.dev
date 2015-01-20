627a1411a0136ea508181cc6d9f8b24815137255
Felice Malaszowski <felice.vargas@gmail.com>
2015-01-20 15:43:17 -0600

    Added classes and instanciations

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,52 +1,20 @@
-<?php
-    // $todo_array = [];
-    //     'Invade Poland',
-    //     'Remove the government',
-    //     'Establish a monarchy',
-    //     'Name myself Queen',
-    //     'Exploit the country\'s natural resources'
-    // ];
+<?php 
+require_once 'filestore.php';
 
-    $filename = 'todo_list.txt';
+
+    $fileStore = new Filestore('todo_list.txt');
     
-    // $newItems = openFile($filename);
-    // $items = array_merge($filename, $newItems);
-
-    function saveFile($filename, $array)
-    {
-        $handle = fopen($filename, 'w');
-            foreach ($array as $item) {
-                fwrite($handle, $item . PHP_EOL);
-            }
-        fclose($handle);
-    }
-
-    function openFile($filename) 
-    {
-        // $contentsArray = [];
-        
-        if (filesize($filename) == 0) {
-            return [];
-        } else {
-            $handle = fopen($filename, 'r');
-            $contents = fread($handle, filesize($filename));
-            $contentsArray = explode("\n", trim($contents));
-            fclose($handle);
-            return $contentsArray;
-        }
-    } 
-    
-    $todo_array = openFile($filename); // List to show on website
+    $todo_array = $fileStore->readLines(); // List to show on website
 
     if(isset($_POST['add_item'])) {
         $todo_array[] = htmlentities(strip_tags($_POST['add_item']));  
-        saveFile($filename, $todo_array); // todo_list.txt
+        $fileStore->writeLines($fileStore->filename, $todo_array); // todo_list.txt
     }
 
     if(isset($_GET['remove'])) {
         $id = $_GET['remove'];
         unset($todo_array[$id]);
-        saveFile($filename, $todo_array); // todo_list.txt
+        $fileStore->writeLines($fileStore->filename, $todo_array); // todo_list.txt
     }
 
     // Verify there were uploaded files and no errors
@@ -58,7 +26,6 @@
         $uploaded_file = basename($_FILES['file1']['name']);
 
         if (substr($uploaded_file, -3) != "txt") {
-            // echo filetype($uploaded_file);
             echo "Please upload only '.txt' files.  " . PHP_EOL;
             echo "Hit your browser's back button to continue.";
             exit();
@@ -69,12 +36,12 @@
 
         // Move the file from the temp location to our uploads directory
         move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
-        $todo_array2 = openFile($savedFilename);
+        // create a new Filestore instance for your uploaded file
+        $var = new fileStore($savedFilename);
+        // use that NEW instance to read in your lines
+        $todo_array2 = $var->readLines($savedFilename);
         $todo_array = array_merge($todo_array, $todo_array2);
-        // var_dump($savedFilename);
-        // var_dump($todo_array);
-        // var_dump($todo_array2);
-        saveFile($filename, $todo_array);
+        $fileStore->writeLines($fileStore->filename, $todo_array);
     }    
 
 ?>
@@ -92,13 +59,8 @@
              echo "<li>{$value} | <a href='/todo_list.php?remove={$key}'>Completed</a></li>";
             }
         ?>        
-        <!-- <li>Invade Poland</li>
-        <li>Remove the government</li>
-        <li>Establish a monarchy</li>
-        <li>Name myself Queen</li>
-        <li>Exploit the country's natural resources</li> -->
     </ul>
-        <!-- create a form that contains the necessary inputs to add a TODO item to the list.  -->
+        <!--create a form that contains the necessary inputs to add a TODO item to the list.-->
     <h2 class = "header-color-and-underline">Add to the list</h2>
     <form method="POST" action="/todo_list.php">  
     
@@ -124,11 +86,7 @@
         </p>
     </form>
     <?= // alternative php syntax 12/22/2014
-    // Check if we saved a file
-    // if (isset($savedFilename)) {
-        // If we did, show a link to the uploaded file
-         "<p> You can grab a copy of your updated file <a href='/{$filename}'>here</a>. Just refresh the page.</p>";
-    // }
+         "<p> You can grab a copy of your updated file <a href='/{$fileStore->filename}'>here</a>. Just refresh the page.</p>";
     ?>
 </body>
 </html>
